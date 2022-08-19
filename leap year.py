51b85d6115eb913b9065c6a0e92492975852a1cc
Shivams9 <91686685+Shivams9@users.noreply.github.com>
2022-08-20 01:27:12 +0530

    Update leap year.py

--- leap year.py
+++ leap year.py
@@ -1,4 +1,8 @@
-year=2016
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+
+#year=2016=Leap Year
+year=int(input())
 ly=4
 if year % 400==0 or year%4==0 and year%100!=0:
     print("leap")
