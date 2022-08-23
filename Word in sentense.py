ee72eac9e294493f93673def3ee5141ed4f6297c
Shivams9 <91686685+Shivams9@users.noreply.github.com>
2022-08-23 15:21:19 +0530

    Create Word in sentense.py

---
+++ Word in sentense.py
@@ -0,0 +1,20 @@
+#This is a sentensec :
+
+
+sentence = "This is a "
+# print(sentence)
+sentence = " " + sentence.strip() + " "
+# print(sentence)
+letters = "abcdefhijklmnopqrstuvwxyz" + "abcdefhijklmnopqrstuvwxyz".upper()
+n = len(sentence)
+startpos = -1
+for i in range(n - 1):
+    current = sentence[i]
+    next = sentence[i + 1]
+    # print(not (current in letters) and next in letters)
+    if not (current in letters) and next in letters:
+        print("Start ", current, next, i + 1)
+        startpos = i
+    if not (next in letters) and current in letters:
+        print("End ", current, next, i)
+        print(sentence[startpos:i + 1].strip())
