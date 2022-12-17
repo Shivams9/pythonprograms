dc61b8646b260c56d5a04aba4f1b97855088e967
SHIVAM SINGH <91686685+Shivams9@users.noreply.github.com>
2022-12-17 17:43:12 +0530

    Create ifelse condition.py

---
+++ ifelse condition.py
@@ -0,0 +1,10 @@
+city= input('Enter city name:')
+scity=city.strip()
+if scity=='Varanasi':
+    print('helo Varanasi..Mahadev')
+elif scity=='chandigarh':
+    print('Hello panjabi..Wahee_Guru')
+elif scity=='seateal':
+    print('hey.. Abhishek')
+else:
+    print('Your City name is invalid')
